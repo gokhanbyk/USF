0cd16d1e0b2f197328b5062e9b8b9d1310b84de5
Gökhan Bıyıkoğlu <gokhan_byk@icloud.com>
2023-10-31 18:35:26 +0300

    modeller adminler falan yapıldı

--- information/admin.py
+++ information/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'title') 
+
+class PostAdmin(admin.ModelAdmin):
+    list_display =(
+        'pk',
+        'title',
+        'category',
+    )
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Post, PostAdmin)
